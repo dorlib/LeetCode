e52417f3adf3e14971f2a579e7429566559354e9
Dor Liberman <90474428+dorlib@users.noreply.github.com>
2022-07-26 20:16:42 +0300

    Update climbStaris.go

--- climbStaris.go
+++ climbStaris.go
@@ -23,3 +23,21 @@ func climbStairsReq(n int, mem map[int]int ) int {
 
 	return res1 + res2
 }
+
+
+// second solution 
+
+func climbStairs(n int) int {
+    if n < 3 { return n }
+    
+    dp := make([]int, n + 1)
+    dp[0] = 0
+    dp[1] = 1
+    dp[2] = 2
+    
+    for i := 3; i <= n; i++ {
+        dp[i] = dp[i-1] + dp[i-2]
+    }
+    
+    return dp[n]
+}
