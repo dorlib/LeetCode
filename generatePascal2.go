f642853c53a9d2667f243ad4a4c8fadb08c296fc
Dor Liberman <90474428+dorlib@users.noreply.github.com>
2022-08-06 12:52:46 +0300

    Create generatePascal2.go

---
+++ generatePascal2.go
@@ -0,0 +1,31 @@
+func getRow(rowIndex int) []int {
+    return generate(rowIndex + 1)[rowIndex]
+}
+
+
+func generate(numRows int) [][]int {
+    result := [][]int{{1}}
+    
+    if numRows == 1 {
+        return result
+    }
+    
+    if numRows >= 2 {
+        result = append(result, []int{1,1})
+    }
+    
+    res := []int{1}
+    
+    for i:= 3; i < numRows + 1; i++ {
+        above := result[i -2]
+        for j := 0; j < i - 2; j++ {
+            res = append(res, above[j] + above[j+1])
+        }
+        res = append(res,1)
+        result = append(result,res)
+        res = []int{1}
+
+    }
+    
+    return result
+}
