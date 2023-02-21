fd214df30d63b09dcc25e5fe9a71c29d882ed60f
Dor Liberman <90474428+dorlib@users.noreply.github.com>
2023-02-21 23:38:43 +0200

    Create ParkingSystem.cs

---
+++ ParkingSystem.cs
@@ -0,0 +1,44 @@
+public class ParkingSystem {
+
+    public int big;
+    public int medium;
+    public int small;
+
+    public ParkingSystem(int big, int medium, int small) {
+        this.big = big;
+        this.medium = medium;
+        this.small = small;
+    }
+    
+    public bool AddCar(int carType) {
+        switch (carType) {
+            case 1:
+                if (this.big > 0) {
+                    this.big--;
+                    return true;
+                }
+                return false;
+
+            case 2:
+                if (this.medium > 0) {
+                    this.medium--;
+                    return true;
+                }
+                return false;
+
+            case 3:
+                if (this.small > 0) {
+                    this.small--;
+                    return true;
+                }
+                return false;
+        }
+         return false;
+    }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj.AddCar(carType);
+ */
